832fcee52bc5c7e2f6c70f441388d1648c00142e
fevico <ajayifavour81@gmail.com>
2024-11-06 00:18:40 +0100

    refactor become a seller endpoint

--- src/seller/seller.controller.ts
+++ src/seller/seller.controller.ts
@@ -1,35 +1,53 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards } from '@nestjs/common';
 import { SellerService } from './seller.service';
 import { AuthenticationGuard } from 'src/guards/Authentication';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { BecomeSellerDto } from './schema/dto/seller.dto';
+import { BecomeSellerDto } from './dto/seller.dto';
 
 @Controller('seller')
-@ApiTags("Seller")
+@ApiTags('Seller')
 export class SellerController {
+  constructor(private sellerService: SellerService) {}
 
-    constructor(private sellerService: SellerService) {}
-
-    @Post('become-seller')
-    @UseGuards(AuthenticationGuard)
-    @ApiOperation({ summary: 'Request seller account verification' })
-    @ApiBody({
-      description: 'Email of the user requesting to become a seller',
-      type: BecomeSellerDto,
-    })
-    @ApiResponse({
-      status: 201,
-      description: 'Verification token sent to your email',
-      schema: {
-        example: {
-          message: 'Verification token sent to your email',
-          token: 'generatedToken12345',
-        },
+  @Post('become-seller')
+  // @UseGuards(AuthenticationGuard)
+  @ApiOperation({ summary: 'Request seller account verification' })
+  @ApiBody({
+    description: 'Email of the user requesting to become a seller',
+    type: BecomeSellerDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Verification token sent to your email',
+    schema: {
+      example: {
+        message: 'Verification token sent to your email',
+        token: 'generatedToken12345',
       },
-    })
-    @ApiResponse({ status: 404, description: 'User not found' })
-    async becomeSeller(@Body() body: BecomeSellerDto) {
-      return this.sellerService.becomeSeller(body);
-    }  
-  
+    },
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  async becomeSeller(@Body() body: BecomeSellerDto) {
+    return this.sellerService.becomeSeller(body);
+  }
+
+  @Post('register')
+  @ApiOperation({ summary: 'Register as a seller' })
+  @ApiBody({
+    description: 'Email of the user requesting to become a seller',
+    type: BecomeSellerDto,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Seller account created successfully',
+    schema: {
+      example: {
+        message: 'Seller account created successfully',
+      },
+    },
+  })
+  @ApiResponse({ status: 404, description: 'User not found' })
+  async register(@Body() body: BecomeSellerDto) {
+    // return this.sellerService.register(body);
+  }
 }
--- src/seller/seller.service.ts
+++ src/seller/seller.service.ts
@@ -1,19 +1,24 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/auth/schema/auth.schema';
 import { Seller } from 'src/seller/schema/seller.schema';
 import { generateToken } from 'src/utils/token';
 import { SellerVerificationToken } from './schema/sellerToken';
-import { BecomeSellerDto } from './schema/dto/seller.dto';
+import { BecomeSellerDto } from './dto/seller.dto';
 
 @Injectable()
 export class SellerService {
   constructor(
-    @InjectModel (Seller.name) private readonly sellerModel:Model<Seller>,
+    @InjectModel(Seller.name) private readonly sellerModel: Model<Seller>,
     @InjectModel(User.name) private userModel: Model<User>,
-    @InjectModel(SellerVerificationToken.name) private sellerVerificationModel: Model<SellerVerificationToken>,
-  ){}
+    @InjectModel(SellerVerificationToken.name)
+    private sellerVerificationModel: Model<SellerVerificationToken>,
+  ) {}
 
   async becomeSeller(body: BecomeSellerDto) {
     const { email } = body;
@@ -21,17 +26,16 @@ export class SellerService {
       throw new BadRequestException('Email is required');
     }
     const user = await this.userModel.findOne({ email });
-    
+
     if (!user) {
       throw new NotFoundException('User not found');
     }
     const token = generateToken();
     await this.sellerVerificationModel.create({ token, owner: user._id });
-    
+
     // Assuming sendEmailVerification is a function to send email notifications
-    // await sendEmailVerification(user.email, token); 
-    
+    // await sendEmailVerification(user.email, token);
+
     return { message: 'Verification token sent to your email', token };
   }
-  
 }
