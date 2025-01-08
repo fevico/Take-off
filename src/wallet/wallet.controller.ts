65675aae4c28b4422d911fadfd477d9c0248b55e
fevico <ajayifavour81@gmail.com>
2025-01-08 20:44:20 +0100

    find user wallet

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { WalletService } from './wallet.service';
+import { Request } from 'express';
+import { AuthenticationGuard } from 'src/guards/Authentication';
+import { AuthorizationGuard } from 'src/guards/Authorization';
+import { Roles } from 'src/decorator/role.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('wallet')
-export class WalletController {}
+@ApiTags('Wallet')
+export class WalletController {
+
+    constructor(private walletService: WalletService ) {}
+
+    @Roles(["admin", "seller"])
+    @Get('user-wallet')
+    @UseGuards(AuthenticationGuard, AuthorizationGuard)
+    async getUserWallet(@Req() req: Request) {
+        const userId = req.user.id;
+        return this.walletService.getUserWallet(userId);
+    }
+}
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Wallet } from './schema/wallet';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+    constructor(@InjectModel(Wallet.name) private readonly walletModel:Model<Wallet>) {}
+
+    async getUserWallet(userId: string) {
+        const wallet = await this.walletModel.findOne({owner:userId});
+        if(!wallet) throw new NotFoundException("No wallet found for this user!")
+            return {wallet : wallet.balance};
+    }
+}
